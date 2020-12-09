15ddd0bd6c26921ec682dae6f5c556087291a02b
Alekscostam <kowalski121.al@gmail.com>
2020-12-09 09:28:20 +0100

    resize image and adding .gitignore

--- main.py
+++ main.py
@@ -5,30 +5,30 @@ import os
 from PIL import Image, ImageTk
 
 
+
+xCanvasSize = 300
+yCanvasSize = 300
+
 screen = tk.Tk()
 screen.title("My app")
 screen.geometry("500x500")
 
 
-canvas = Canvas(screen, width = 300, height = 300, bg="white") 
+canvas = Canvas(screen, width = xCanvasSize, height = yCanvasSize, bg="white") 
 canvas.pack(pady=20)
-
-
 
 def select_image():
     filepath = filedialog.askopenfilename()
-    img = ImageTk.PhotoImage(Image.open(filepath))     
-    canvas.create_image(20,20, anchor=NW, image=img)    
-    canvas.image = img 
+    image=Image.open(filepath)
+    image = image.resize((xCanvasSize, yCanvasSize))  
+    photoImage = ImageTk.PhotoImage(image)
+    canvas.create_image(0,0, anchor=NW, image=photoImage)    
+    canvas.image = photoImage 
    
  
 
-
-
 button = tk.Button(text="Upload image", command=select_image)
 button.pack()
-
-
 
 
 screen.mainloop()
